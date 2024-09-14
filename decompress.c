ccc9627300e6cef3d014d5299b5e79de603cc239
jostinrl8 <86120943+jostinrl8@users.noreply.github.com>
2024-09-14 12:54:29 -0600

    Update decompress.c

--- decompress.c
+++ decompress.c
@@ -56,6 +56,42 @@ FILE* readBinFile(char* filename){
     return compressed;
 }
 
+//Función para leer el encabezado del archivo comprimido
+void readPositions(FILE* compressed, FileLinkedList *positions){
+    //Obtiene los caracteres extra del encabezado
+    char c;
+    char extraChar[64] = {0};
+    while((c = fgetc(compressed)) != '@'){
+        strncat(extraChar, &c, 1);
+    }
+
+    //Obtiene la cantidad de archivos del encabezado
+    char cantFiles[64] = {0};
+    while((c = fgetc(compressed)) != '@'){
+        strncat(cantFiles, &c, 1);
+    }
+
+    char position[512] = {0};
+    int files = atoi(cantFiles);
+    int cantExtra = atoi(extraChar);
+
+    //Lee y guarda las posiciones de cada libro
+    int cont = 0;    
+    while(cont < files){
+        while((c = fgetc(compressed)) != '@'){
+            strncat(position, &c, 1);
+        }
+        if(c == '@'){
+            addFileNode(positions, " ", atoi(position) + cantExtra);
+            memset(position, 0, sizeof(position));
+        }
+        else{
+            strncat(position, &c, 1);
+        }
+        cont++;
+    }
+}
+
 //Función para consultar y/o crear una carpeta
 void consultFolder(char* folder){
     struct stat st = {0};
@@ -69,17 +105,6 @@ void consultFolder(char* folder){
 
 void decompressFile(FILE *compressed, Node *root, char *folder, int fileNumber)
 {
-    /*int contAt = 0;
-    char at; 
-    while(contAt != fileNumber){
-        at = fgetc(compressed);
-        if(at == '@'){
-            contAt++;
-        }
-    }*/
-
-   //printf("File number: %d\n", fileNumber);
-
     fseek(compressed, fileNumber, SEEK_SET);
 
     char c;
@@ -104,8 +129,6 @@ void decompressFile(FILE *compressed, Node *root, char *folder, int fileNumber)
     strcpy(url, folder);
     strcat(url, "/");
     strcat(url, filename);
-
-    //printf("Descomprimiendo archivo: %s\n", url);
 
     FILE *decompressed = fopen(url, "w");
     if (decompressed == NULL)
@@ -140,159 +163,51 @@ void decompressFile(FILE *compressed, Node *root, char *folder, int fileNumber)
     fclose(decompressed);
 }
 
-void decompressFiles(FILE *compressed, Node *root, char *folder)
-{
-    while (!feof(compressed))
-    {
-        // Leer encabezado
-        char c;
-        char name[512] = {0};
-        while ((c = fgetc(compressed)) != '@' && c != EOF)
-        {
-            strncat(name, &c, 1);
-        }
-        if (c == EOF)
-        {
-            break;
-        }
-
-        // Obtener solo el nombre del archivo a partir de la ruta completa
-        char *filename = strrchr(name, '/');
-        if (filename != NULL)
-        {
-            filename++;
-        }
-        else
-        {
-            filename = name;
-        }
-
-        char url[512];
-        strcpy(url, folder);
-        strcat(url, "/");
-        strcat(url, filename);
-
-        FILE *decompressed = fopen(url, "w");
-        if (decompressed == NULL)
-        {
-            printf("Error al crear el archivo de descompresión\n");
-            fclose(compressed);
-            exit(1);
-        }
-
-        // Leer y descomprimir el contenido
-        char code[100] = {0};
-        int cont = 0;
-        while ((c = fgetc(compressed)) != EOF)
-        {
-            if (c == '@')
-            {
-                break;
-            }
-            code[cont] = c;
-            code[cont + 1] = '\0';
-            cont++;
-
-            char character = getCharacter(root, code);
-            if (character != '\0')
-            {
-                fprintf(decompressed, "%c", character);
-                memset(code, 0, sizeof(code));
-                cont = 0;
-            }
-        }
-
-        fclose(decompressed);
-    }
-}
-
 void serial_decompress(char *filename, char *folder)
 {
     FILE* compressed = readBinFile(filename);
     FileLinkedList positions;
     positions.head = NULL;
 
-    char c;
-    char extraChar[64] = {0};
-    while((c = fgetc(compressed)) != '@'){
-        strncat(extraChar, &c, 1);
-    }
-
-    char cantFiles[64] = {0};
-    while((c = fgetc(compressed)) != '@'){
-        strncat(cantFiles, &c, 1);
-    }
-
-    char position[512] = {0};
-    int files = atoi(cantFiles);
-
-    int cantExtra = atoi(extraChar);
-
-    //printf("Extra char: %s\n", extraChar);
-    //printf("Cantidad de archivos: %d\n", files);
-    int cont = 0;    
-    while(cont < files){
-        while((c = fgetc(compressed)) != '@'){
-            strncat(position, &c, 1);
-        }
-        if(c == '@'){
-            addFileNode(&positions, " ", atoi(position) + cantExtra);
-            memset(position, 0, sizeof(position));
-        }
-        else{
-            strncat(position, &c, 1);
-        }
-        cont++;
-    }
-
+    readPositions(compressed, &positions);
     Node* root = readTree(compressed);
     consultFolder(folder);
-
-    //decompressFiles(compressed, root, folder);
     
+    //Descomprimir todos los archivos
     FileNode* current = positions.head;
     while(current->next != NULL){
         decompressFile(compressed, root, folder, current->position);
         current = current->next;
     }
-    //decompressFile(compressed, root, folder, positions.head->next->position);
-    //decompressFile(compressed, root, folder, 0);
-    // delete the file temp.txt
  
     fclose(compressed);
     return;
 }
 
 void fork_decompress(char* filename, char* folder){
+    FileLinkedList positions;
+    positions.head = NULL;
+    
+    // Abrir el archivo comprimido y leer las posiciones y el árbol
     FILE* compressed = readBinFile(filename);
+    readPositions(compressed, &positions);
     Node* root = readTree(compressed);
+    fclose(compressed); 
+
     consultFolder(folder);
 
-    int fileNumber = 0;
-    //while (fileNumber < 177) {
+    FileNode* current = positions.head;
+    while(current != NULL){
         pid_t pid = fork();
         if (pid == 0) {
-            // Proceso hijo
-            printf("Proceso hijo número: %d\n", fileNumber);
-            decompressFile(compressed, root, folder, fileNumber);
-            //fclose(compressed);
+            FILE* compressed_child = readBinFile(filename);
+            decompressFile(compressed_child, root, folder, current->position);
+            fclose(compressed_child);
             exit(0);
-        } else if (pid > 0) {
-            // Proceso padre
-            fileNumber += 2;
-        } else {
-            printf("Error al crear el proceso hijo");
-            exit(1);
-        }
-    //}
-
-    /*// Esperar a que todos los procesos hijos terminen
-    while (fileNumber > 0) {
-        wait(NULL);
-        fileNumber -= 2;
-    }*/
-
-    fclose(compressed);
+        } 
+        current = current->next;
+    }
+    
     return;
 }
 
